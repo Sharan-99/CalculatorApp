01460b9f44efe24e1fb2d55b7c2585c3e09eb5fd
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 09:09:44 +0530

    first commit

---
+++ CalculatorLib.Tests/CalculatorTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CalculatorLib;
+
+namespace CalculatorLib.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [TestCase]
+        public void Add_When_Both_Inputs_GreaterThanZero_Returns_ExpectedResult()
+        {
+            //Arrange
+            Calculator calculator = new Calculator();
+
+            int firstNo = 30;
+            int secondNo = 20;
+
+            int expectedResult = 50;
+
+            int actualResult;
+
+            //Act
+
+            actualResult = calculator.Add(firstNo, secondNo);
+
+            //Assert
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+           
+        }
+
+        [Test]
+        public void Add_When_FirstInputGreaterThanZero_SecondInputLessThanOrEqualToZero_Returns_ExpectedResult()
+        {
+            //Arrange
+            Calculator calculator = new Calculator();
+
+            int firstNo = 30;
+            int secondNo = 0;
+
+            int expectedResult = -1;
+
+            int actualResult;
+
+            //Act
+
+            actualResult = calculator.Add(firstNo, secondNo);
+
+            //Assert
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void Add_When_SecondInputGreaterThanZero_FirstInputLessThanOrEqualToZero_Returns_ExpectedResult()
+        {
+            //Arrange
+            Calculator calculator = new Calculator();
+
+            int firstNo = -5;
+            int secondNo = 20;
+
+            int expectedResult = -2;
+
+            int actualResult;
+
+            //Act
+
+            actualResult = calculator.Add(firstNo, secondNo);
+
+            //Assert
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void Add_When_Both_Inputs_LessThanOrEqualToZero_Returns_ExpectedResult()
+        {
+            //Arrange
+            Calculator calculator = new Calculator();
+
+            int firstNo = -5;
+            int secondNo = 0;
+
+            int expectedResult = 0;
+
+            int actualResult;
+
+            //Act
+
+            actualResult = calculator.Add(firstNo, secondNo);
+
+            //Assert
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+
+    }
+}
---
+++ CalculatorLib/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLib
+{
+    public class Calculator
+    {
+        public int Add(int firstNum,int secondNum)
+        {
+            if (firstNum > 0 && secondNum > 0)
+                return firstNum + secondNum;
+            else if (firstNum > 0 && secondNum <= 0)
+                return -1;
+            else if (firstNum <= 0 && secondNum > 0)
+                return -2;
+            else
+                return 0;
+        }
+    }
+}
